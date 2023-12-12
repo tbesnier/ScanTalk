f78fea2548c5f346409e7b11372f569d220ac1ae
Federico Nocentini <federico.nocentini@unifi.it>
2023-12-12 16:46:58 +0100

    Update data_loader_diffusion_net.py

--- data_loader_diffusion_net.py
+++ data_loader_diffusion_net.py
@@ -9,7 +9,8 @@ from transformers import Wav2Vec2Processor
 import librosa
 from wav2vec import Wav2Vec2Model
 import sys
-sys.path.append('/home/federico/Scrivania/ST/ScanTalk/model/diffusion-net/src')
+import trimesh
+sys.path.append('/home/federico/Scrivania/ST/ScanTalk/model/diffusion-net_/src')
 import diffusion_net
 
 
@@ -35,7 +36,8 @@ class Dataset(data.Dataset):
         evecs = self.data[index]["evecs"]
         gradX = self.data[index]["gradX"]
         gradY = self.data[index]["gradY"]
-        return torch.FloatTensor(audio), torch.FloatTensor(vertices), torch.FloatTensor(template), torch.FloatTensor(np.array(mass)).float(), L.float(), torch.FloatTensor(np.array(evals)), torch.FloatTensor(np.array(evecs)), gradX.float(), gradY.float(), file_name
+        faces = self.data[index]["faces"]
+        return torch.FloatTensor(audio), torch.FloatTensor(vertices), torch.FloatTensor(template), torch.FloatTensor(np.array(mass)).float(), L.float(), torch.FloatTensor(np.array(evals)), torch.FloatTensor(np.array(evecs)), gradX.float(), gradY.float(), file_name, faces.float()
 
     def __len__(self):
         return self.len
@@ -51,40 +53,85 @@ def read_data(args):
     audio_path = args.wav_path
     vertices_path = args.vertices_path
     processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+    #audio_encoder = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h")
+    #audio_encoder.feature_extractor._freeze_parameters()
     
-    template_file = args.template_file
-    with open(template_file, 'rb') as fin:
-        templates = pickle.load(fin, encoding='latin1')
+    if args.dataset == "vocaset":
+        reference = trimesh.load('/home/federico/Scrivania/ST/ScanTalk/template/flame_model/FLAME_sample.ply', process=False)
+        template_tri = reference.faces
+        template_file = args.template_file
+        with open(template_file, 'rb') as fin:
+            templates = pickle.load(fin, encoding='latin1')
+            
+    elif args.dataset == "BIWI":
+        reference = trimesh.load('/home/federico/Scrivania/ST/Data/Biwi/aligned_downsampled_templates/F1.obj', process=False)
+        template_tri = reference.faces
+    
     k=0
+    subject_id_list = []
+    mass_dict = {}
+    L_dict = {}
+    evals_dict = {}
+    evecs_dict = {}
+    gradX_dict = {}
+    gradY_dict = {}
     for r, ds, fs in os.walk(audio_path):
         for f in tqdm(fs):
-            if f.endswith("wav"):
+            if f.endswith("wav") and f[3] != 'e':
                 wav_path = os.path.join(r, f)
                 speech_array, sampling_rate = librosa.load(wav_path, sr=16000)
                 audio_feature = np.squeeze(processor(speech_array, sampling_rate=16000).input_values)
-                #audio_feature = np.reshape(audio_feature, (-1, audio_feature.shape[0]))
-                #audio_feature = torch.FloatTensor(audio_feature)
                 key = f.replace("wav", "npy")
                 data[key]["audio"] = audio_feature
-                subject_id = "_".join(key.split("_")[:-1])
-                temp = templates[subject_id]
                 data[key]["name"] = f
-                data[key]["template"] = temp
+                
+                if args.dataset == "vocaset":
+                    subject_id = "_".join(key.split("_")[:-1])
+                    temp = templates[subject_id]
+                    data[key]["template"] = temp
+                    
+                elif args.dataset == "BIWI":
+                    subject_id = key.split("_")[0]
+                    temp =trimesh.load(os.path.join(args.template_file, subject_id + '.obj'), process=False).vertices
+                    data[key]["template"] = temp
+
                 vertices_path_ = os.path.join(vertices_path, f.replace("wav", "npy"))
-                _, mass, L, evals, evecs, gradX, gradY = diffusion_net.geometry.compute_operators(torch.tensor(temp), faces=None, k_eig=128)
-                data[key]["mass"] = mass
-                data[key]["L"] = L
-                data[key]["evals"] = evals
-                data[key]["evecs"] = evecs
-                data[key]["gradX"] = gradX
-                data[key]["gradY"] = gradY
+                
+                if subject_id not in subject_id_list:
+                    subject_id_list.append(subject_id)
+                    #frame, mass, L, evals, evecs, gradX, gradY = diffusion_net.geometry.compute_operators(torch.tensor(temp), faces=None, k_eig=128)
+                    frame, mass, L, evals, evecs, gradX, gradY = diffusion_net.geometry.compute_operators(torch.tensor(temp), faces=torch.tensor(template_tri), k_eig=128)
+                    mass_dict[subject_id] = mass
+                    L_dict[subject_id] = L
+                    evals_dict[subject_id] = evals
+                    evecs_dict[subject_id] = evecs
+                    gradX_dict[subject_id] = gradX
+                    gradY_dict[subject_id] = gradY
+               
+                data[key]["mass"] = mass_dict[subject_id]
+                data[key]["L"] = L_dict[subject_id]
+                data[key]["evals"] = evals_dict[subject_id]
+                data[key]["evecs"] = evecs_dict[subject_id]
+                data[key]["gradX"] = gradX_dict[subject_id]
+                data[key]["gradY"] = gradY_dict[subject_id]
+                data[key]["faces"] = torch.tensor(template_tri)
+                
 
                 if not os.path.exists(vertices_path_):
                     del data[key]
                 else:
-                    vertices = np.load(vertices_path_, allow_pickle=True)[::2, :]
-                    data[key]["vertices"] = np.reshape(vertices, (vertices.shape[0], 5023, 3))
-                    
+                    if args.dataset == "vocaset":
+                        vertices = np.load(vertices_path_, allow_pickle=True)[::2, :]
+                        data[key]["vertices"] = np.reshape(vertices, (vertices.shape[0], 5023, 3))
+                    elif args.dataset == "BIWI":
+                        #vertices = np.load(vertices_path_, allow_pickle=True)
+                        data[key]["vertices"] = np.load(vertices_path_, allow_pickle=True)
+                        
+                #audio_feature = np.reshape(audio_feature, (-1, audio_feature.shape[0]))
+                #audio_feature = torch.FloatTensor(audio_feature)
+                #data[key]["audio"] = audio_encoder(audio_feature, frame_num=len(data[key]["vertices"])).last_hidden_state
+                        
+                        
                 #k += 1
                 #if k == 5:
                 #    break
@@ -94,16 +141,16 @@ def read_data(args):
     subjects_dict["val"] = [i for i in args.val_subjects.split(" ")]
     subjects_dict["test"] = [i for i in args.test_subjects.split(" ")]
 
-    splits = {'train': range(1, 41), 'val': range(1, 41), 'test': range(1, 41)}
+    #splits = {'train': range(1, 41), 'val': range(1, 41), 'test': range(1, 41)}
 
     for k, v in data.items():
         subject_id = "_".join(k.split("_")[:-1])
         sentence_id = int(k.split(".")[0][-2:])
-        if subject_id in subjects_dict["train"] and sentence_id in splits['train']:
+        if subject_id in subjects_dict["train"]:
             train_data.append(v)
-        if subject_id in subjects_dict["val"] and sentence_id in splits['val']:
+        if subject_id in subjects_dict["val"]:
             valid_data.append(v)
-        if subject_id in subjects_dict["test"] and sentence_id in splits['test']:
+        if subject_id in subjects_dict["test"]:
             test_data.append(v)
 
     print(len(train_data), len(valid_data), len(test_data))
